51307aff828c53c4666cc2c0936cbc8a80fc79a3
Frankie Sardo <fran.sardo@gmail.com>
2013-08-27 19:27:49 +0100

    Update release.sh

--- release.sh
+++ release.sh
@@ -3,13 +3,13 @@ IP=0.0.0.0
 FILENAME=Main
 
 # Java: 
-# javac PLAYERNAME/FILNENAME.java
+# javac $PLAYERNAME/$FILNENAME.java
 
 # Scala: 
-# scalac -d PLAYERNAME/ PLAYERNAME/FILNENAME.scala
+# scalac -d $PLAYERNAME/$FILNENAME.scala
 
 # C#
-# gmcs PLAYERNAME/FILNENAME.cs
+# gmcs $PLAYERNAME/$FILNENAME.cs
 
 zip -r $PLAYERNAME.zip ./$PLAYERNAME
 curl http://$IP:3000/players --data-binary @$PLAYERNAME.zip
